7fbc53449264b5cc53a41071fc86b0f9d3418a45
awhetzel <awhetzel@localhost.localdomain>
2018-09-09 18:23:04 -0600

    added some styling

--- mysite/app1/forms.py
+++ mysite/app1/forms.py
@@ -3,11 +3,11 @@ from .models import Company, Employee, Dependent
 from django.forms.models import modelformset_factory
 
 class CalcForm(forms.Form):
-    emp_salary = forms.DecimalField(label='Salary')
+    emp_salary = forms.CharField(label='Salary')
     base_deduction = forms.CharField(label='Base Deduction')
-    dependents = forms.IntegerField(label='Dependents')
-    per_dependent = forms.DecimalField(label='Deduction/Dependent')
-    discount = forms.DecimalField(label='Discount Percentage')
+    dependents = forms.CharField(label='Dependents')
+    per_dependent = forms.CharField(label='Deduction/Dependent')
+    discount = forms.CharField(label='Discount Percentage')
 
 
 DependentFormSet = modelformset_factory(Dependent, exclude=())
