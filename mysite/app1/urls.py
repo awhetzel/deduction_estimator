4d33afd737abc3c4e410e6e3a46d41494ccf4b34
awhetzel <awhetzel@localhost.localdomain>
2018-09-16 21:40:04 -0600

    cleaning house and styling

--- mysite/app1/urls.py
+++ mysite/app1/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('employee/<int:pk>', views.EmployeeDetailView.as_view(), name='employee-detail'),
